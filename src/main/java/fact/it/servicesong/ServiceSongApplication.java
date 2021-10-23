d2345fa6ff3197535d7db6f1688eeef4f3926847
anthonydecap <r0805615@student.thomasmore.be>
2021-10-23 12:51:30 +0200

    init

---
+++ src/main/java/fact/it/servicesong/ServiceSongApplication.java
@@ -0,0 +1,13 @@
+package fact.it.servicesong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/servicesong/ServiceSongApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.servicesong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
